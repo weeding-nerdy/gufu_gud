33a02ffbd29ab4279ac5fa336987676b78afef53
weeding-nerdy <droidicus@engineer.com>
2020-12-20 16:47:42 -0500

    Adding debug messaging, optimizing ADC params

--- gg_interface/src/main.h
+++ gg_interface/src/main.h
@@ -12,11 +12,17 @@
 #define INIT_ATTEMPTS 5
 #define SERIAL_BAUD 115200
 #define JSON_BUFFER_SIZE 64
-#define I2C_BUS_SPEED 4e2 * 1e3
+#define JSON_BUFFER_SIZE_DEBUG (10 * JSON_BUFFER_SIZE)
+#define I2C_BUS_SPEED (4e2 * 1e3)
+
+// INA260 configuration
 #define CONVERSION_READY_MASK 0x400
+#define INA260_CONVERSION_TIME INA260_TIME_140_us
+#define INA260_AVG_COUNT INA260_COUNT_4
 
 // Protos
 void alert(void);
+void debug_msg(String msg);
 
 // Reset function to simulate POR
 #define RESTART_ADDR 0xE000ED0C
